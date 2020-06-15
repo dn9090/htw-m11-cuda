7120cf0e695cd8860811bc24b251dc3c6712a9fa
dn9090 <s0559090@htw-berlin.de>
2020-06-15 12:57:03 +0200

    Add script for running the cuda build

---
+++ run_cuda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+make cuda
+
+bin/image_modifier_cuda grey examples/example_image1_small.png export/example_image1_small_grey.png
+bin/image_modifier_cuda blur examples/example_image1_small.png export/example_image1_small_blur.png
+bin/image_modifier_cuda hsv examples/example_image1_small.png export/example_image1_small_hsv.png
+
+
